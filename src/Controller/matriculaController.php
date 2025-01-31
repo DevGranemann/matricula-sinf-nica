e191c398702d91e33381b446e4e58948a9857106
macbook <macbook@MacBook-Air-de-Eduardo.local>
2025-01-30 23:12:24 -0300

    Dando andamento no projeto

--- src/Controller/matriculaController.php
+++ src/Controller/matriculaController.php
@@ -12,42 +12,43 @@ class matriculaController extends AbstractController
     #[Route('/', name:'app_index')]
     public function index(): Response
     {
-        $categories =
-        [
-            ['title' => 'Aluno',         'text' => 'Matrícula de Aluno'],
-            ['title' => 'Curso',         'text' => 'Matrícula de Curso'],
-            ['title' => 'Cadastro',       'text' => 'Cadastro de Aluno'],
-
-        ];
+        
 
         $pageTitle = "Menu";
         return $this->render('home/home.html.twig', [
         
-            'categories' => $categories,
             'pageTitle' => $pageTitle,
         ]);
     }
     #[Route('/aluno', name:'app_aluno')]
     public function aluno(string $slug=null): Response
     {    
-        $categories =
-        [
-            ['title' => 'Aluno',         'text' => 'Matrícula de Aluno'],
-            ['title' => 'Curso',         'text' => 'Matrícula de Curso'],
-            ['title' => 'Cadastro',       'text' => 'Cadastro de Aluno'],
-            
-        ];
+        
         
         
         $pageTitle = "";
         return $this->render('aluno.html.twig', [
             
-            'categories' => $categories,
-            'pageTitle' => $pageTitle,
+        ]);
+
+
+    }
+    #[Route('/curso', name:'app_curso')]
+    public function curso(string $slug=null): Response
+    {
+        return $this->render('curso.html.twig', [
+
 
 
         ]);
 
 
+    }
+    #[Route('/cadastro', name:'app_cadastro')]
+    public function cadastro(string $slug=null): Response
+    {
+        return $this-> render('cadastro.html.twig', [
+
+        ]);
     }
 }
