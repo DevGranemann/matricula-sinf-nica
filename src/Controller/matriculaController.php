7697523a5831d88ca55980bf10001ef5533cf8fa
macbook <macbook@MacBook-Air-de-Eduardo.local>
2025-02-02 15:25:54 -0300

    Obtendo os dados inseridos pelo usuário; colocando-os no BD; mostrando eles numa TALBE

--- src/Controller/matriculaController.php
+++ src/Controller/matriculaController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Aluno;
+use App\Form\AlunoType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class matriculaController extends AbstractController
@@ -12,25 +16,55 @@ class matriculaController extends AbstractController
     #[Route('/', name:'app_index')]
     public function index(): Response
     {
-        
-
-        $pageTitle = "Menu";
         return $this->render('home/home.html.twig', [
-        
-            'pageTitle' => $pageTitle,
+          
         ]);
     }
-    #[Route('/aluno', name:'app_aluno')]
-    public function aluno(string $slug=null): Response
+    #[Route('/aluno', name:'cadastro_aluno')]
+    public function aluno(Request $request, EntityManagerInterface $em): Response
     {    
-        
-        
-        
-        $pageTitle = "";
+
+        // criando uma nova instância de Aluno
+        $aluno = new Aluno();
+
+        // criando o formulário
+        $form = $this->createForm(AlunoType::class, $aluno);
+
+        // processando o formulário
+        $form->handleRequest($request);
+
+        // verificando se o formulário foi enviado e é válido
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            //se for verdade então salva o aluno no banco
+            $em->persist($aluno);
+            $em->flush();
+
+            //é redidirecionado para tabela de alunos cadastrados
+            return $this->redirectToRoute('ver_aluno');
+        }
+
+        //renderiza o form na view
         return $this->render('aluno.html.twig', [
-            
+
+            'form' => $form->createView(),
+
         ]);
 
+
+    }
+    #[Route('/aluno/ver_aluno', name:'ver_aluno')]
+    public function vizualizar(EntityManagerInterface $em): Response
+    {
+
+        //busca todos os aluno no banco
+        $alunos = $em->getRepository(Aluno::class)->findAll();
+
+        //Renderiza a view com tabela de alunos
+        return $this->render('ver_aluno.html.twig',[
+            'alunos' => $alunos,
+
+        ]);
 
     }
     #[Route('/curso', name:'app_curso')]
@@ -45,7 +79,7 @@ class matriculaController extends AbstractController
 
     }
     #[Route('/cadastro', name:'app_cadastro')]
-    public function cadastro(string $slug=null): Response
+    public function cadastro(): Response
     {
         return $this-> render('cadastro.html.twig', [
 
