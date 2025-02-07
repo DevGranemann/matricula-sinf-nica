ac464eaec70556dfb0a221754abd3ece87ce1962
macbook <macbook@MacBook-Air-de-Eduardo.local>
2025-02-06 22:52:37 -0300

    adicionei uma migration e alterei a entity 'Aluno' :D

---
+++ migrations/Version20250206185348.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250206185348 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE aluno CHANGE idade idade VARCHAR(2) NOT NULL, CHANGE cpf cpf VARCHAR(11) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE aluno CHANGE idade idade VARCHAR(3) NOT NULL, CHANGE cpf cpf INT NOT NULL');
+    }
+}
